e5aa341321127cf5aff6af3722e41ac94f1a5650
ok3ks <freelanceokeks@gmail.com>
2023-10-31 18:42:53 +0000

    Refactored to OOP

--- app/report.py
+++ app/report.py
@@ -1,54 +1,59 @@
-from utils import get_player, get_points, get_gw_transfers, get_participant_entry, obtain_league_participants, weekly_score
-from utils import basic_stats
+from utils import League, Player, Gameweek
+from utils import get_participant_entry, get_gw_transfers
 import pandas as pd
 import numpy as np
 
-one_results_list = []
+#if __name__ == "__main__":
 gw = 10
 LEAGUE_ID = 1088941
 
-entries_df = obtain_league_participants(LEAGUE_ID)
-participants_json = {item['entry']:item['player_name'] for item in entries_df}
+league = League(LEAGUE_ID)
+league.obtain_league_participants
+print(league.obtain_league_participants())
+one_results_list = league.get_all_participant_entries(gw)
 
-for item in participants_json.keys():
-    one_results_list.append(get_participant_entry(item, gw))
+df = Gameweek(gw)
+df.weekly_score()
 
-df = weekly_score(gw)
 one_df = pd.DataFrame(one_results_list)
 o_df = one_df[~one_df['players'].isna()]
+#l
 
-o_df['points_breakdown'] = o_df['players'].map(lambda x: [get_points(y, gw, df) for y in x.split(",")])
-o_df['captain_points'] = o_df['captain'].map(lambda x: get_points(x, gw, df) * 2)
-o_df['vice_captain_points'] = o_df['vice_captain'].map(lambda x: get_points(x, gw, df))
+o_df['points_breakdown'] = o_df['players'].map(lambda x: [df.get_points(y) for y in x.split(",")])
+o_df['captain_points'] = o_df['captain'].map(lambda x: df.get_points(x) * 2)
+o_df['vice_captain_points'] = o_df['vice_captain'].map(lambda x: df.get_points(x))
 o_df['rank'] = o_df['total_points'].rank(ascending=False)
 o_df['rank'] = o_df['rank'].map(int)
 
-row = get_gw_transfers(participants_json.keys(), 9)
+#row = get_gw_transfers(participants_json.keys(), gw)
+row = league.get_gw_transfers(gw)
+print(row)
 f = pd.DataFrame(row)
 f = f.T
 
+#print(f)
 #f.reset_index()
 #f.drop(axis ='Index', index= 'entry_id', inplace= True)
 
-f['transfer_points_in'] = f['element_in'].map(lambda x: sum([get_points(y, gw,df) for y in x]))
-f['transfer_points_out'] = f['element_out'].map(lambda x:sum([get_points(y, gw,df) for y in x]))
-
+f['transfer_points_in'] = f['element_in'].map(lambda x: sum([df.get_points(y) for y in x]))
+f['transfer_points_out'] = f['element_out'].map(lambda x:sum([df.get_points(y) for y in x]))
 o_df['entry'] = o_df['entry'].astype(int)
 o_df.rename(columns={'entry':'entry_id'}, inplace= True)
 
 #Detecting Exceptional and Abysmal performances 
-Q1,league_average,Q3 = basic_stats(df['total_points'])
+Q1,league_average,Q3 = df.basic_stats()
 IQR = Q3 - Q1
-
 exceptional = o_df[o_df['total_points'] > Q3 +1.5*IQR]
 abysmal = o_df[o_df['total_points'] < Q1 - 1.5*IQR]
 
+#
 f['transfers'] = f['element_out'].map(lambda x: len(x))
 f['delta'] = f['transfer_points_in'] - f['transfer_points_out']
 f.reset_index(inplace= True)
 f.rename(columns= {'index': 'entry_id'}, inplace= True)
 f = o_df.merge(f, on='entry_id', how='right')
 
+#
 counts = f['element_out'].value_counts().reset_index().to_dict('list')
 most_transf_out = [(counts['element_out'][i], counts['index'][i]) for i in range(3)]
 least_transf_out = [(counts['element_out'][-i], counts['index'][-i]) for i in range(1,4)]
@@ -57,12 +62,13 @@ counts = f['element_in'].value_counts().reset_index().to_dict('list')
 most_transf_in = [(counts['element_in'][i], counts['index'][i]) for i in range(3)]
 least_transf_in = [(counts['element_in'][-i], counts['index'][-i]) for i in range(1,4)] #because -0 == 0
 
+#
 captain = o_df['captain'].value_counts().to_dict()
 chips = o_df['active_chip'].value_counts().to_dict()
-
 no_chips = f[f['active_chip'].isna()]
 no_chips = no_chips.sort_values(by = 'delta', ascending=False)
 
+###Print to terminal -- Work on next
 best_transf_in = get_player(no_chips[no_chips['delta'] == max(no_chips['delta'])]['element_out'].values[0][0])
 best_transf_out = get_player(no_chips[no_chips['delta'] == max(no_chips['delta'])]['element_in'].values[0][0])
 best_transf_points = max(no_chips['delta'])
@@ -73,16 +79,13 @@ worst_transf_points = min(no_chips['delta'])
 
 #write to json
 ##compute then display
-
-
-
 print("{} Made at least one transfer".format(len(f))) #use percentage to report
 print("{} participants took a hit".format(len(f[f['transfers'] > 1])))
 print(f"\nBest transfer decision of the week ")
 print("\t")
 
 for i in range(3):
-    player_in = get_player(no_chips.iloc[i,:]['element_in'])
+    player_in = get_player(no_chips.iloc[i,:]['element_in']) #
     player_out = get_player(no_chips.iloc[i,:]['element_out'])
     points_gained = no_chips.iloc[i,:]['delta']
     player_id = no_chips.iloc[i,:]['entry_id']
--- app/utils.py
+++ app/utils.py
@@ -4,93 +4,19 @@ import pandas as pd
 import json
 import numpy as np
 
-transfer_url = 'https://fantasy.premierleague.com/api/entry/{}/transfers/'
-league_url = "https://fantasy.premierleague.com/api/leagues-classic/{}/standings/?page_standings={}"
-fpl_player = "https://fantasy.premierleague.com/api/entry/{}/event/{}/picks/"
-gw_url = "https://fantasy.premierleague.com/api/event/{}/live/"
-
+from urls import GW_URL,FIXTURE_URL,TRANSFER_URL, HISTORY_URL
+from urls import H2H_LEAGUE, LEAGUE_URL, FPL_PLAYER
+from functools import lru_cache
 
 with open('json/epl_players.json') as ins_3:
     epl_players = json.load(ins_3)
-
-def weekly_score(gw):
-
-    r = requests.get(gw_url.format(gw))
-    r = r.json()
-
-    temp = {item['id'] :item['stats'] for item in r['elements']}
-    df = pd.DataFrame(temp)
-    df = df.T
-
-    df.columns = ['minutes',
-        'goals_scored',
-        'assists',
-        'clean_sheets',
-        'goals_conceded',
-        'own_goals',
-        'penalties_saved',
-        'penalties_missed',
-        'yellow_cards',
-        'red_cards',
-        'saves',
-        'bonus',
-        'bps',
-        'influence',
-        'creativity',
-        'threat',
-        'ict_index',
-        'starts',
-        'expected_goals',
-        'expected_assists',
-        'expected_goal_involvements',
-        'expected_goals_conceded',
-        'total_points',
-        'in_dreamteam']
-
-    df.reset_index(level= 0, names = 'id', inplace = True)
-    df['event'] = gw
-
-    return df
-
-def get_points(id, gw,df): #extract from DB
-    """Obtains player points using ID"""
-
-    if id == "NaN":
-        print("{id} not found in db for week {gw}")
-        pass
-    else:
-        assert gw in df['event'], 'All_df is not updated'
-        temp_df = df[df['event'] == gw] #gw
-        point = temp_df[temp_df['id'] == int(id)]['total_points'].values.tolist()
-        #player = id_2_player.get(i)         #change to database
-        return int(point[0]) 
-
-def basic_stats(df):
-
-    """Measures of Central Tendency for Total points"""
-    average = np.mean(df['total_points'])
-    IQR = np.percentile(df['total_points'], 75) - np.percentile(df['total_points'], 25)
-    Q3 = np.percentile(df['total_points'], 75)
-    Q1 = np.percentile(df['total_points', 25])
-
-    return average,Q3,Q1,IQR
-
-def get_player(i):
-    """Obtains player name from id"""
-    out = []
-    if isinstance(i, list):
-        for item in i:
-            out.append(epl_players.get(str(item)))
-        return out
-    else:
-        return epl_players.get(str(i))
 
 def get_gw_transfers(alist,gw:int, all = False):
     """Input is a list of entry_id. Gw is the gameweek number
     All toggles between extracting all gameweeks or not"""
     row =  {}
     for entry_id in alist:
-        r = requests.get(transfer_url.format(entry_id))
+        r = requests.get(TRANSFER_URL.format(entry_id))
 
         if r.status_code == 200:
             obj = r.json()
@@ -115,13 +41,10 @@ def get_gw_transfers(alist,gw:int, all = False):
 
                         row[item['entry']]['element_in'].append(item['element_in'])
                         row[item['entry']]['element_out'].append(item['element_out'])     
-
     return row
         
-
-#
 def get_participant_entry(entry_id, gw):
-    r = requests.get(fpl_player.format(entry_id, gw))
+    r = requests.get(FPL_PLAYER.format(entry_id, gw))
     #Logs
     print("Retrieving results, participant {} for event = {}".format(entry_id, gw))
 
@@ -163,25 +86,143 @@ def get_participant_entry(entry_id, gw):
     #time.sleep(3)
     return team_list
 
-#Leagues
-def obtain_league_participants(league_id):
-    """ This function uses the league url as an endpoint to query for participants of a league at a certain date.
-    Should be used to update participants table in DB """
+class Gameweek(): 
     
-    has_next = True
-    entries = []
-    PAGE_COUNT = 1
+    def __init__(self, gw:int):
+        self.gw = gw
 
-    while has_next:
-        r = requests.get(league_url.format(league_id, PAGE_COUNT))
-        obj =r.json()
+    def weekly_score(self):
+        r = requests.get(GW_URL.format(self.gw))
+        r = r.json()
 
-        assert r.status_code == 200, 'error connecting to the endpoint'
-        del r
-        entries.extend(obj['standings']['results'])
-        has_next = obj['standings']['has_next']
-        PAGE_COUNT += 1
-        time.sleep(2)
-    return entries
+        temp = {item['id']:item['stats'] for item in r['elements']}
+        self.df = pd.DataFrame(temp)
+        self.df = self.df.T
+        self.df.columns = ['minutes',
+            'goals_scored',
+            'assists',
+            'clean_sheets',
+            'goals_conceded',
+            'own_goals',
+            'penalties_saved',
+            'penalties_missed',
+            'yellow_cards',
+            'red_cards',
+            'saves',
+            'bonus',
+            'bps',
+            'influence',
+            'creativity',
+            'threat',
+            'ict_index',
+            'starts',
+            'expected_goals',
+            'expected_assists',
+            'expected_goal_involvements',
+            'expected_goals_conceded',
+            'total_points',
+            'in_dreamteam']
 
-#get weekly points from api
+        self.df.reset_index(level= 0, names = 'id', inplace = True)
+        self.df['event'] = self.gw
+        return self.df
+
+    def get_points(self, id): #extract from DB
+        """Obtains player points using ID"""
+        if id == "NaN":
+            print("{id} not found in db for week {gw}")
+            pass
+        else:
+            assert int(id) in self.df['id'].astype(int), 'All_df is not updated'
+            point = self.df[self.df['id'] == int(id)]['total_points'].values.tolist()
+            return int(point[0]) 
+
+    def basic_stats(self):
+        """Measures of Central Tendency for Total points"""
+        average = np.mean(self.df['total_points'])
+        Q3 = np.percentile(self.df['total_points'], 75)
+        Q1 = np.percentile(self.df['total_points'], 25)
+        return Q1,average,Q3
+
+#Create Player class for name, position, team
+class Player():
+    #Use DB for this - fairly constant, but start with json
+
+    def __init__(self,player_id, epl_players:dict):
+        self.player_id = player_id
+        self.epl_players = epl_players
+
+    def get_player_name(self):
+        """Obtains player name from id"""
+        out = []
+        if isinstance(self.player_id, list):
+            for item in self.player_id:
+                out.append(epl_players.get(str(item)))
+            return out
+        else:
+            return epl_players.get(str(self.player_id))
+        
+    def get_player_position(self):
+        pass
+
+    def get_player_team(self):
+        pass
+
+    def get_player_fixture(self):
+        pass
+
+    #def get_player_attr(self):
+        """Outputs Needed attributes at once"""
+        #if isinstance(self.player_id, list):
+            #for item in self.player_id:
+                #attribute_tuple = (get_player_name(item), get_player_position(item), 
+                                  #get_player_team(item))
+                #out.append(attribute_tuple)
+            #return out
+        #else:
+            #return epl_players.get(str(self.player_id))
+        pass
+
+class League():
+    def __init__(self, league_id):
+        self.league_id = league_id
+        self.participants = []
+
+    def obtain_league_participants(self):
+        """This function uses the league url as an endpoint to query for participants of a league at a certain date.
+        Should be used to update participants table in DB """
+        has_next = True
+        PAGE_COUNT = 1
+        while has_next:
+            r = requests.get(LEAGUE_URL.format(self.league_id, PAGE_COUNT))
+            obj =r.json()
+            assert r.status_code == 200, 'error connecting to the endpoint'
+            del r
+            self.participants.extend(obj['standings']['results'])
+            has_next = obj['standings']['has_next']
+            PAGE_COUNT += 1
+            time.sleep(2)
+        return self.participants
+    
+    def get_all_participant_entries(self,gw):
+        self.obtain_league_participants()
+        self.participant_entries = [get_participant_entry(participant['entry'],gw) for participant in self.participants]
+        return self.participant_entries
+    
+    def get_gw_transfers(self,gw):
+        if len(self.participants) > 1:
+            self.entry_ids =[participant['entry'] for participant in self.participants]
+            self.transfers = get_gw_transfers(self.entry_ids,gw)
+        else:
+            self.obtain_league_participants()
+            self.transfers = get_gw_transfers(self.entry_ids,gw)
+        return self.transfers
+
+#Participant class
+
+class Participant():
+    def __init__(self,entry_id, player_name, entry_name, gw_total):
+        self.entry_id = entry_id
+        self.player_name = player_name
+        self.entry_name = entry_name
+        self.gw_total = gw_total
