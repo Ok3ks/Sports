9cc5b00dbc46600ccc659e3528bd14c1f0a9f20d
ok3ks <freelanceokeks@gmail.com>
2023-12-11 06:35:07 +0000

    Modified paths

--- src/paths.py
+++ src/paths.py
@@ -1,7 +1,10 @@
 import os
 
-BASE_DIR = os.path.realpath(os.path.curdir) #make this absolute
+BASE_DIR = os.path.realpath(os.path.dirname(os.path.dirname(__file__))) #make this absolute
+SRC_DIR = os.path.join(BASE_DIR, 'src')
 APP_DIR = os.path.join(BASE_DIR, 'app')
-REPORT_DIR = os.path.join(APP_DIR, 'reports')
+REPORT_DIR = os.path.join(BASE_DIR, 'reports')
 WEEKLY_REPORT_DIR = os.path.join(REPORT_DIR, 'weekly_report')
-FPL_WRAP_DIR = os.path.join(REPORT_DIR, 'fpl_wrap')
+
+if __name__ == "__main__":
+    print("{}\n{}\n{}\n{}\n{}".format(BASE_DIR,SRC_DIR, APP_DIR, REPORT_DIR, WEEKLY_REPORT_DIR))
