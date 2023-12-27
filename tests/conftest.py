eef55ed496c0c0a76b59d4736031c57cc31212f3
ok3ks <freelanceokeks@gmail.com>
2023-12-27 22:13:23 +0000

    Added tests for Participant and League classes

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,16 +3,51 @@ from src.paths import REPORT_DIR
 import os
 from os.path import join,realpath
 
+from src.utils import Participant
+
+
 @pytest.fixture(scope="module")
-def create_league():
-    return
+def league_fixture():
+    return int(1088941)
+
+@pytest.fixture(scope = "module")
+def league_participants():
+    return [{'id': 37378749, 'event_total': 36, 'player_name': 'Emmanuel Okedele', 'rank': 1, 'last_rank': 2, 'rank_sort': 1, 'total': 1100, 'entry': 98120, 'entry_name': 'Potters Touch'},\
+    {'id': 38949786, 'event_total': 26, 'player_name': 'Adedapo Adedire', 'rank': 2, 'last_rank': 3, 'rank_sort': 2, 'total': 1088, 'entry': 1086398, 'entry_name': 'Sylarexx Fc'}, \
+    {'id': 37386221, 'event_total': 19, 'player_name': 'Uncle Buzzey', 'rank': 3, 'last_rank': 1, 'rank_sort': 3, 'total': 1084, 'entry': 293449, 'entry_name': 'Coyg'},\
+    {'id': 62483704, 'event_total': 28, 'player_name': 'Star Boy', 'rank': 4, 'last_rank': 4, 'rank_sort': 4, 'total': 1058, 'entry': 456141, 'entry_name': 'Akaza Dono'}, \
+    {'id': 47033862, 'event_total': 15, 'player_name': 'Martins Omoniyi', 'rank': 5, 'last_rank': 5, 'rank_sort': 5, 'total': 1044, 'entry': 3572212, 'entry_name': 'Bulldozers FC'},\
+    {'id': 37391869, 'event_total': 30, 'player_name': 'moyosola junior', 'rank': 6, 'last_rank': 8, 'rank_sort': 6, 'total': 1041, 'entry': 32133, 'entry_name': 'olympique mayonnaise'}, \
+    {'id': 67447801, 'event_total': 19, 'player_name': 'Progress Akintade', 'rank': 7, 'last_rank': 6, 'rank_sort': 7, 'total': 1038, 'entry': 4145862, 'entry_name': 'PROGRESSS'}, \
+    {'id': 45353356, 'event_total': 24, 'player_name': 'coker sunkanmi', 'rank': 8, 'last_rank': 7, 'rank_sort': 8, 'total': 1037, 'entry': 4834604, 'entry_name': 'Sujeyinc'}, \
+    {'id': 37437413, 'event_total': 25, 'player_name': 'Vicapo Awosika', 'rank': 9, 'last_rank': 10, 'rank_sort': 9, 'total': 1033, 'entry': 124922, 'entry_name': 'Vicapo FC'}, \
+    {'id': 39299443, 'event_total': 31, 'player_name': 'Femi Sobodu', 'rank': 10, 'last_rank': 11, 'rank_sort': 10, 'total': 1032, 'entry': 973439, 'entry_name': 'Chronicles Of VARnia'}, \
+    {'id': 53769740, 'event_total': 22, 'player_name': 'Gbemileke David', 'rank': 11, 'last_rank': 9, 'rank_sort': 11, 'total': 1027, 'entry': 4917316, 'entry_name': 'Aare Ika CF'}, \
+    {'id': 37598256, 'event_total': 24, 'player_name': 'John Dere', 'rank': 12, 'last_rank': 14, 'rank_sort': 12, 'total': 998, 'entry': 4858268, 'entry_name': 'Lord Madara'}, \
+    {'id': 43227690, 'event_total': 8, 'player_name': 'Matthew Omoniyi', 'rank': 13, 'last_rank': 12, 'rank_sort': 13, 'total': 997, 'entry': 5493191, 'entry_name': 'FPL Juggernaut'}, \
+    {'id': 51006805, 'event_total': 16, 'player_name': 'Abideen Ayangbemi', 'rank': 14, 'last_rank': 13, 'rank_sort': 14, 'total': 992, 'entry': 483899, 'entry_name': 'Dafuq FC'}, \
+    {'id': 37627137, 'event_total': 24, 'player_name': 'Chukwudi Onyewuchi', 'rank': 15, 'last_rank': 16, 'rank_sort': 15, 'total': 990, 'entry': 2929610, 'entry_name': 'Zups!'}, \
+    {'id': 38275547, 'event_total': 43, 'player_name': 'Ayodeji Omoniyi', 'rank': 16, 'last_rank': 19, 'rank_sort': 16, 'total': 980, 'entry': 34174, 'entry_name': 'God Abeg…'}, \
+    {'id': 40102684, 'event_total': 3, 'player_name': 'chisom ndianefo', 'rank': 17, 'last_rank': 15, 'rank_sort': 17, 'total': 973, 'entry': 5144052, 'entry_name': 'Eventually!'}, \
+    {'id': 47799952, 'event_total': 31, 'player_name': 'Iam KayY', 'rank': 18, 'last_rank': 18, 'rank_sort': 18, 'total': 968, 'entry': 1118096, 'entry_name': 'KayY'}, \
+    {'id': 37475582, 'event_total': 28, 'player_name': 'Uncle Tunes', 'rank': 19, 'last_rank': 20, 'rank_sort': 19, 'total': 964, 'entry': 805203, 'entry_name': 'Shikamaru Fergie FC'}, \
+    {'id': 52412247, 'event_total': 17, 'player_name': 'Oyeniyi Okusi', 'rank': 20, 'last_rank': 17, 'rank_sort': 20, 'total': 958, 'entry': 1551370, 'entry_name': 'Manchachi'}, \
+    {'id': 48580891, 'event_total': 14, 'player_name': 'Emmanuel Nnaemeka', 'rank': 21, 'last_rank': 21, 'rank_sort': 21, 'total': 902, 'entry': 5104192, 'entry_name': 'Emiradofc'}, \
+    {'id': 37612408, 'event_total': 33, 'player_name': 'Peterclever Nnodim', 'rank': 22, 'last_rank': 23, 'rank_sort': 22, 'total': 884, 'entry': 1552881, 'entry_name': 'Cashflow FC'}, \
+    {'id': 43398060, 'event_total': 13, 'player_name': 'Reginald Maduka', 'rank': 23, 'last_rank': 22, 'rank_sort': 23, 'total': 878, 'entry': 5509203, 'entry_name': 'Bullion van'}]
+
+@pytest.fixture(scope = "module")
+def league_fill_fixture():
+    return [{'id': 37378749, 'event_total': 36, 'player_name': 'Emmanuel Okedele', 'rank': 1, 'last_rank': 2, 'rank_sort': 1, 'total': 1100, 'entry': 98120, 'entry_name': 'Potters Touch'},\
+    {'id': 38949786, 'event_total': 26, 'player_name': 'Adedapo Adedire', 'rank': 2, 'last_rank': 3, 'rank_sort': 2, 'total': 1088, 'entry': 1086398, 'entry_name': 'Sylarexx Fc'}, \
+    {'id': 37386221, 'event_total': 19, 'player_name': 'Uncle Buzzey', 'rank': 3, 'last_rank': 1, 'rank_sort': 3, 'total': 1084, 'entry': 293449, 'entry_name': 'Coyg'}]
 
 @pytest.fixture(scope="module")
 def filepath():
     return realpath(REPORT_DIR)
 
 @pytest.fixture(scope="module")
-def player():
+def participant():
     return int(98120)
 
 @pytest.fixture(scope="module")
@@ -28,8 +63,8 @@ def gw_fixture():
     return int(8)
 
 @pytest.fixture(scope="module")
-def diff_gw_fixture():
-    return [8,10]
+def span_fixture():
+    return [8,10,3]
 
 @pytest.fixture(scope="module")
 def values():
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,15 @@
 from src.utils import get_basic_stats,get_gw_transfers, parse_transfers, get_participant_entry, to_json
 import pytest
+
 from os.path import join, realpath
 import os
 import json
+
+from tests.test_endpoints import test_fpl_url_endpoint
+from src.utils import check_gw, Participant, get_curr_event,League
+
+from src.urls import FPL_URL,TRANSFER_URL
+import requests
 
 def test_to_json(transfer_obj, filepath):
     output_name = 'test.json'
@@ -21,9 +28,8 @@ def test_get_basic_stats(values):
     assert round(average, 2) == 7.75 
     assert Q3  == 11.75
 
-def test_parse_transfers(player, transfer_obj):
+def test_parse_transfers(transfer_obj):
     
-    #test_transfer_endpoint(player)
     row = parse_transfers(transfer_obj)
     row_keys = list(row.keys())
 
@@ -32,13 +38,39 @@ def test_parse_transfers(player, transfer_obj):
     assert row[98120]['element_in'] == [transfer_obj['element_in']]
     assert row[98120]['element_out'] == [transfer_obj['element_out']]
 
-def test_get_diff_gw_transfers(player, diff_gw_fixture):
-    row = get_gw_transfers([player], diff_gw_fixture)
-    assert list(row[diff_gw_fixture[0]].keys())[0] == player
-    assert set(row.keys()).union(set(diff_gw_fixture)) == set(diff_gw_fixture)
 
-def test_get_all_gw_transfers(player,diff_gw_fixture):
-    row = get_gw_transfers([player], diff_gw_fixture, all = True)
+def test_check_gw_int_is_true(gw_fixture):
+    assert check_gw(gw_fixture)[0] == True , 'Only 38 games in a season'
+    assert check_gw(gw_fixture)[1] == 8
+
+def test_check_gw_span_is_true(span_fixture):
+    assert check_gw(span_fixture)[0] == True
+    assert check_gw(span_fixture)[1] == [8, 10, 3]
+
+@pytest.mark.parametrize("diff_fixture", [40] )
+def test_check_gw_is_false(diff_fixture):
+    assert check_gw(diff_fixture) == None , 'Only 38 games in a season'
+
+def test_get_curr_event():
+    r = requests.get(FPL_URL)
+    assert r.status_code == 200, 'Endpoint unavailable, check participant_id and gameweek'
+
+    r = r.json()    
+    assert type(r) == dict
+    assert 'events' in r.keys()
+
+    check_set = r['events'][0]
+    inter_ = set(['finished', 'data_checked', 'id', 'is_current'])
+
+    assert inter_.intersection(check_set) == inter_
+
+def test_get_diff_gw_transfers(participant, span_fixture):
+    row = get_gw_transfers([participant], span_fixture)
+    assert list(row[span_fixture[0]].keys())[0] == participant
+    assert set(row.keys()).union(set(span_fixture)) == set(span_fixture)
+
+def test_get_all_gw_transfers(participant,diff_gw_fixture):
+    row = get_gw_transfers([participant], diff_gw_fixture, all = True)
     keys = list(row.keys())
 
     start = keys[-1]
@@ -47,9 +79,9 @@ def test_get_all_gw_transfers(player,diff_gw_fixture):
 
     assert set(row.keys()).union(rang) == set(rang) 
 
-def test_get_participant_entry(player, gw_fixture):
+def test_get_participant_entry(participant, gw_fixture):
 
-    team_list = get_participant_entry(player, gw_fixture)
+    team_list = get_participant_entry(participant, gw_fixture)
     team_list_keys = list(team_list.keys())
 
     assert "gw" in team_list_keys
@@ -58,9 +90,116 @@ def test_get_participant_entry(player, gw_fixture):
     assert "points_on_bench" in team_list_keys
     assert "event_transfers_cost" in team_list_keys
     assert "captain" in team_list_keys
-    assert "players" in team_list_keys
+    assert "participants" in team_list_keys
     assert "bench" in team_list_keys
+    assert "auto_subs" in team_list_keys
 
+    assert len(team_list['players']) == 11 , 'Onfield players must be 11'
+    assert len(team_list['bench']) == 5, "Bench players must be 4"
+    assert len(team_list['captain']) == 1, "One captain"
+    assert len(team_list['vice_captain']) == 1, "One vice captain"
+
+class TestParticipant():
+
+    def test_init(self,participant):
+        test = Participant(participant)
+        assert test.participant == 98120
+
+    def test_get_gw_transfers(self,participant, gw_fixture):
+        test = Participant(participant)
+        output = test.get_gw_transfers(gw_fixture)
+        
+        assert type(output) == dict
+        assert gw_fixture in list(output.keys())
+
+        elems_keys = output[gw_fixture].keys()
+        elems = set(['element_in', 'element_out'])
+        
+        assert elems.union(elems_keys) == elems
+        output_not_all_not_check_gw = test.get_gw_transfers(39,all = False)
+        
+        assert type(output_not_all_not_check_gw) == dict
+        assert len(output_not_all_not_check_gw.keys()) == 0
+    
+    def test_get_span_week_transfers(self,participant, span_fixture):
+        test = Participant(participant)
+        output = test.get_span_week_transfers(span_fixture)
+
+        assert type(output) == dict
+        event_keys = list(output.keys())
+        span_set = set(span_fixture)
+
+        assert span_set.union(event_keys) == span_set
+
+    def test_get_all_week_transfers(self,participant):
+        test = Participant(participant)
+        curr_gw = get_curr_event()
+        curr_gw = curr_gw[0]
+
+        output = test.get_all_week_transfers()
+
+        assert type(output) == dict
+        #some weeks transfers are saved, so assertions cannot work 
+        #event_keys = set(list(output.keys()))
+        #all_keys = [i for i in range(1, curr_gw+1)]
+        #assert event_keys.union(all_keys) == event_keys
+
+    def test_get_all_week_entries():
+        pass
+
+    def test_participant_get_all_week_entries():
+        pass
+
+    def test_participant_get_span_week_transfers():
+        pass
+
+    def test_participant_get_all_week_transfers():
+        pass
+
+class TestLeague():
+    
+    def test_init(self, league_fixture):
+        test = League(league_fixture)
+        assert test.league_id == 1088941
+        assert test.participants == [] 
+        pass
+
+    def test_league_obtain_league_participants_empty(self):
+        #Endpoint tests validates this
+        pass
+
+    def test_league_obtain_league_participants_fill(self,league_fixture,league_fill_fixture):
+        test = League(league_fixture)
+        test.participants = league_fill_fixture
+
+        obj = test.obtain_league_participants()
+        keys = set(['entry', 'entry_name', 'id', 'event_total', 'player_name', 'rank', 'last_rank', 'rank_sort', 'total'])
+
+        diff = keys.difference(test.participants[0].keys())
+        assert keys.intersection(test.participants[0].keys()) == keys, f"Vital keys missing, Add keys -  {diff}"
+
+        assert len(test.participants) == len(obj)
+        assert type(test.participants) == list
+        
+        assert test.entry_ids != None
+        assert type(test.entry_ids) == list
+
+    
+    def test_league_get_participant_name(self,league_fixture,league_fill_fixture):
+        test = League(league_fixture)
+        test.participants = league_fill_fixture
+        names = test.get_participant_name()
+
+        assert 'entry' in test.participants[0].keys()
+        assert 'entry_name' in test.participants[0].keys()
+    
+        assert type(names) == dict
+
+    def test_league_get_all_participant_entries():
+        pass
+
+    def test_league_get_gw_transfers():
+        pass
 
 if __name__ == "__main__":
    print("use pytest to run tests")
