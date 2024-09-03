11e3d94984e22ce040e48214cc5c822d3282efdd
Emmanuel Okedele <emmanuel@coefficient.ai>
2024-09-03 22:16:56 +0100

    Graphql switched from  Django ORM to sqlAlchemy ORM

--- app/LiveProject/report_app/resolvers.py
+++ app/LiveProject/report_app/resolvers.py
@@ -9,7 +9,7 @@ from ariadne import (
 )
 from django.contrib.auth.hashers import make_password
 from .models import (Players, Gameweek_Scores)
-from src.db.db import get_player
+from src.db.db import get_player,get_player_stats_from_db
 
 # Acknowledgement, Message,
 from .shortcuts import get_object_or_none
@@ -19,30 +19,28 @@ _document = ObjectType("Document")
 
 
 # Query resolvers
-@query.field("players")
-def resolve_players(*_):
-    """Retrieve all Player model instances."""
-    return Players.objects.all()
+# @query.field("players")
+# def resolve_players(*_):
+#     """Retrieve all Player model instances."""
+#     return get_players
 
 
 @query.field("player")
 def resolve_player(*_, id, half):
     """Retrieve a User model instance by ID or return None if not found."""
-    # player = Players.objects.filter(half=half)
-    # player = Players.objects.get(half=half, player_id=id)
     return get_player(id, half)
 
 
 @query.field("playerGameweekScore")
-def resolve_player_gameweek_score(*_, id):
+def resolve_player_gameweek_score(*_, id, gameweek):
     """Retrieve a Player's gameweek score based on player_id"""
-    return get_object_or_none(Gameweek_Scores, pk=id)
+    return get_player_stats_from_db(id, gameweek)
 
+# @query.field("gameweekScore")
+# def resolve_player_gameweek_scores(*_,):
+#     """Retrieve a Player's gameweek score based on player_id"""
+#     return Gameweek_Scores().objects.all
 
-@query.field("gameweekScore")
-def resolve_player_gameweek_scores(*_,):
-    """Retrieve a Player's gameweek score based on player_id"""
-    return Gameweek_Scores().objects.all
 
 # Combine the defined schema and resolvers
 type_defs = load_schema_from_path("./report_app/schema.graphql")
