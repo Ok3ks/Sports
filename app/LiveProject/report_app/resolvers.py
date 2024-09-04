47a7d5f0051944ad59be76a4eeee8b9f6d5483f6
Emmanuel Okedele <emmanuel@coefficient.ai>
2024-09-04 04:14:35 +0100

    wip testing report endpoint

--- app/LiveProject/report_app/resolvers.py
+++ app/LiveProject/report_app/resolvers.py
@@ -7,16 +7,21 @@ from ariadne import (
     load_schema_from_path,
     make_executable_schema,
 )
-from django.contrib.auth.hashers import make_password
-from .models import (Players, Gameweek_Scores)
-from src.db.db import get_player,get_player_stats_from_db
 
-# Acknowledgement, Message,
-from .shortcuts import get_object_or_none
+from django.contrib.auth.hashers import make_password
+# from .models import (Players, Gameweek_Scores)
+from src.db.db import get_player_gql, get_player_stats_from_db_gql
+from src.report import LeagueWeeklyReport
+# from .shortcuts import get_object_or_none
 
 query = QueryType()
 _document = ObjectType("Document")
 
+#For GameView
+# @query.field("gameweekScore")
+# def resolve_player_gameweek_scores(*_,):
+#     """Retrieve a Player's gameweek score based on player_id"""
+#     return Gameweek_Scores().objects.all
 
 # Query resolvers
 # @query.field("players")
@@ -25,22 +30,35 @@ _document = ObjectType("Document")
 #     return get_players
 
 
+#LeagueView
 @query.field("player")
 def resolve_player(*_, id, half):
-    """Retrieve a User model instance by ID or return None if not found."""
-    return get_player(id, half)
+    """Retrieve a Player's information by ID or return None if not found."""
+    return get_player_gql(id, half)
 
 
 @query.field("playerGameweekScore")
 def resolve_player_gameweek_score(*_, id, gameweek):
     """Retrieve a Player's gameweek score based on player_id"""
-    return get_player_stats_from_db(id, gameweek)
+    return get_player_stats_from_db_gql(id, gameweek)
 
-# @query.field("gameweekScore")
-# def resolve_player_gameweek_scores(*_,):
-#     """Retrieve a Player's gameweek score based on player_id"""
-#     return Gameweek_Scores().objects.all
+# query get leagueReport - plug into function
+#if indexed, retrieve, others create and save
 
+
+@query.field("leagueWeeklyReport")
+def resolve_league_gameweek_report(*_, league_id, gameweek):
+    """Retrieve a Player's gameweek score based on player_id"""
+    # return get_player_stats_from_db(id, gameweek)
+    report = LeagueWeeklyReport(gameweek, league_id)
+    report.get_data()
+    report.weekly_score_transformation()
+    report.merge_league_weekly_transfer()
+    report.add_auto_sub()
+    report.captain_minutes()
+    output = report.create_report(display=False)
+    print(output)
+    return output
 
 # Combine the defined schema and resolvers
 type_defs = load_schema_from_path("./report_app/schema.graphql")
