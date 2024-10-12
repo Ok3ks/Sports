d6c74fa9f0870cadedd933cbe519a45eecd48046
Emmanuel Okedele <emmanuel@coefficient.ai>
2024-10-12 02:21:43 +0100

    prod env config

--- auth.sh
+++ auth.sh
@@ -1,10 +1,10 @@
 export DB_PORT='5432'
 export DB_USERNAME='postgres'
 export DB_PASSWORD='temptemptemp'
-export DB_HOST='34.147.195.97'
+export DB_HOST='/cloudsql/<><>'
 export DB_DATABASE="fpl"
 export DB_DRIVER_NAME="postgresql+psycopg2"
 export DJANGO_SECRET_KEY='django-insecure-f&xs472(b!o6_b$5l*--ea1*m^*_=y))edubhvkol3t4%2*#a_'
-export REDIS_HOST="127.0.0.1"
-export REDIS_PORT="6379"
+export REDISHOST
+export REDISPORT
 # export REDIS_PASSWORD=None
