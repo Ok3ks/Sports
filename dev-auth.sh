673f8e04f6bdfce3b8a0e16d04310824f911e3f6
Emmanuel Okedele <emmanuel@coefficient.ai>
2025-01-05 10:48:41 +0000

    dev secrets

--- dev-auth.sh
+++ dev-auth.sh
@@ -1,10 +1,10 @@
 export DB_PORT='5432'
-export DB_USERNAME='postgres'
+# export DB_USERNAME='postgres'
 export DB_PASSWORD='temptemptemp'
-export DB_HOST='/cloudsql/<><>'
+export DB_HOST='127.0.0.1'
 export DB_DATABASE="fpl"
 export DB_DRIVER_NAME="postgresql+psycopg2"
 export DJANGO_SECRET_KEY='django-insecure-f&xs472(b!o6_b$5l*--ea1*m^*_=y))edubhvkol3t4%2*#a_'
-export REDISHOST
-export REDISPORT
+export REDISHOST=localhost
+export REDISPORT="6379"
 # export REDIS_PASSWORD=None
