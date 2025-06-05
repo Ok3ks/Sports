b34c12d8cf1288ce962a21784cb3c8886374fc55
Emmanuel Okedele <emmanuel@coefficient.ai>
2025-06-05 15:06:22 +0100

    no supported opertation for method + int corrected

--- LiveProject/LiveProject/gunicorn.py
+++ LiveProject/LiveProject/gunicorn.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 import multiprocessing
 
 bind = ":8000"
-workers = multiprocessing.cpu_count * 2  + 1
+workers = multiprocessing.cpu_count() *2  + 1
 wsgi_app = "LiveProject.wsgi:application"
 timeout = 300
 max_requests = 120
