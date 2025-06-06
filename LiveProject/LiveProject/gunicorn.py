675b94eaf7b15caafc429cbd3fecb51596079e35
ok3ks <freelanceokeks@gmail.com>
2025-06-06 07:02:25 +0100

    functional deployed image

--- LiveProject/LiveProject/gunicorn.py
+++ LiveProject/LiveProject/gunicorn.py
@@ -1,11 +1,7 @@
 from __future__ import annotations
+
 import multiprocessing
 
-bind = ":8000"
-workers = multiprocessing.cpu_count() *2  + 1
+bind = "0.0.0.0:8000"
+workers = multiprocessing.cpu_count() * 2 + 1
 wsgi_app = "LiveProject.wsgi:application"
-timeout = 300
-max_requests = 120
-threads = 4
-loglevel = 'info'
-
