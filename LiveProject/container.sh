2a053a32dda3bf451492bb15a0a5f615444e252c
Emmanuel Okedele <emmanuel@coefficient.ai>
2025-09-01 14:37:51 +0100

    Master Docker image with update script inbuilt for new deployments

--- LiveProject/container.sh
+++ LiveProject/container.sh
@@ -1,7 +1,79 @@
-for i in {1..38}
-    do
-    python src/db/update_gameweek_score.py -g "$i"
-    done
-
 python src/db/update_player_information.py -ha 1
 python src/db/update_player_information.py -ha 2
+
+python src/db/update_gameweek_score.py -g 1
+python src/db/update_gameweek_score.py -g 2
+python src/db/update_gameweek_score.py -g 3
+python src/db/update_gameweek_score.py -g 4
+python src/db/update_gameweek_score.py -g 5
+python src/db/update_gameweek_score.py -g 6
+python src/db/update_gameweek_score.py -g 7
+python src/db/update_gameweek_score.py -g 8
+python src/db/update_gameweek_score.py -g 9
+python src/db/update_gameweek_score.py -g 10
+python src/db/update_gameweek_score.py -g 11
+python src/db/update_gameweek_score.py -g 12
+python src/db/update_gameweek_score.py -g 13
+python src/db/update_gameweek_score.py -g 14
+python src/db/update_gameweek_score.py -g 15
+python src/db/update_gameweek_score.py -g 16
+python src/db/update_gameweek_score.py -g 17
+python src/db/update_gameweek_score.py -g 18
+python src/db/update_gameweek_score.py -g 19
+python src/db/update_gameweek_score.py -g 20
+python src/db/update_gameweek_score.py -g 21
+python src/db/update_gameweek_score.py -g 22
+python src/db/update_gameweek_score.py -g 23
+python src/db/update_gameweek_score.py -g 24
+python src/db/update_gameweek_score.py -g 25
+python src/db/update_gameweek_score.py -g 26
+python src/db/update_gameweek_score.py -g 27
+python src/db/update_gameweek_score.py -g 28
+python src/db/update_gameweek_score.py -g 29
+python src/db/update_gameweek_score.py -g 30
+python src/db/update_gameweek_score.py -g 31
+python src/db/update_gameweek_score.py -g 32
+python src/db/update_gameweek_score.py -g 33
+python src/db/update_gameweek_score.py -g 34
+python src/db/update_gameweek_score.py -g 35
+python src/db/update_gameweek_score.py -g 36
+python src/db/update_gameweek_score.py -g 37
+python src/db/update_gameweek_score.py -g 38
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
