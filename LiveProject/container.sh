51701ee4b5fd34107341e89d29ef9ba6606acada
ok3ks <freelanceokeks@gmail.com>
2026-06-30 17:42:32 +0100

    try prefixing poetry

--- LiveProject/container.sh
+++ LiveProject/container.sh
@@ -1,45 +1,45 @@
-python src/db/update_player_information.py -ha 1
-python src/db/update_player_information.py -ha 2
-python src/db/update_season_fixture.py
+poetry run python src/db/update_player_information.py -ha 1
+poetry python src/db/update_player_information.py -ha 2
+poetry python src/db/update_season_fixture.py
 
-python src/db/update_gameweek_score.py -g 1
-python src/db/update_gameweek_score.py -g 2
-python src/db/update_gameweek_score.py -g 3
-python src/db/update_gameweek_score.py -g 4
-python src/db/update_gameweek_score.py -g 5
-python src/db/update_gameweek_score.py -g 6
-python src/db/update_gameweek_score.py -g 7
-python src/db/update_gameweek_score.py -g 8
-python src/db/update_gameweek_score.py -g 9
-python src/db/update_gameweek_score.py -g 10
-python src/db/update_gameweek_score.py -g 11
-python src/db/update_gameweek_score.py -g 12
-python src/db/update_gameweek_score.py -g 13
-python src/db/update_gameweek_score.py -g 14
-python src/db/update_gameweek_score.py -g 15
-python src/db/update_gameweek_score.py -g 16
-python src/db/update_gameweek_score.py -g 17
-python src/db/update_gameweek_score.py -g 18
-python src/db/update_gameweek_score.py -g 19
-python src/db/update_gameweek_score.py -g 20
-python src/db/update_gameweek_score.py -g 21
-python src/db/update_gameweek_score.py -g 22
-python src/db/update_gameweek_score.py -g 23
-python src/db/update_gameweek_score.py -g 24
-python src/db/update_gameweek_score.py -g 25
-python src/db/update_gameweek_score.py -g 26
-python src/db/update_gameweek_score.py -g 27
-python src/db/update_gameweek_score.py -g 28
-python src/db/update_gameweek_score.py -g 29
-python src/db/update_gameweek_score.py -g 30
-python src/db/update_gameweek_score.py -g 31
-python src/db/update_gameweek_score.py -g 32
-python src/db/update_gameweek_score.py -g 33
-python src/db/update_gameweek_score.py -g 34
-python src/db/update_gameweek_score.py -g 35
-python src/db/update_gameweek_score.py -g 36
-python src/db/update_gameweek_score.py -g 37
-python src/db/update_gameweek_score.py -g 38
+poetry python src/db/update_gameweek_score.py -g 1
+poetry python src/db/update_gameweek_score.py -g 2
+poetry python src/db/update_gameweek_score.py -g 3
+poetry python src/db/update_gameweek_score.py -g 4
+poetry python src/db/update_gameweek_score.py -g 5
+poetry python src/db/update_gameweek_score.py -g 6
+poetry python src/db/update_gameweek_score.py -g 7
+poetry python src/db/update_gameweek_score.py -g 8
+poetry python src/db/update_gameweek_score.py -g 9
+poetry python src/db/update_gameweek_score.py -g 10
+poetry python src/db/update_gameweek_score.py -g 11
+poetry python src/db/update_gameweek_score.py -g 12
+poetry python src/db/update_gameweek_score.py -g 13
+poetry python src/db/update_gameweek_score.py -g 14
+poetry python src/db/update_gameweek_score.py -g 15
+poetry python src/db/update_gameweek_score.py -g 16
+poetry python src/db/update_gameweek_score.py -g 17
+poetry python src/db/update_gameweek_score.py -g 18
+poetry python src/db/update_gameweek_score.py -g 19
+poetry python src/db/update_gameweek_score.py -g 20
+poetry python src/db/update_gameweek_score.py -g 21
+poetry python src/db/update_gameweek_score.py -g 22
+poetry python src/db/update_gameweek_score.py -g 23
+poetry python src/db/update_gameweek_score.py -g 24
+poetry python src/db/update_gameweek_score.py -g 25
+poetry python src/db/update_gameweek_score.py -g 26
+poetry python src/db/update_gameweek_score.py -g 27
+poetry python src/db/update_gameweek_score.py -g 28
+poetry python src/db/update_gameweek_score.py -g 29
+poetry python src/db/update_gameweek_score.py -g 30
+poetry python src/db/update_gameweek_score.py -g 31
+poetry python src/db/update_gameweek_score.py -g 32
+poetry python src/db/update_gameweek_score.py -g 33
+poetry python src/db/update_gameweek_score.py -g 34
+poetry python src/db/update_gameweek_score.py -g 35
+poetry python src/db/update_gameweek_score.py -g 36
+poetry python src/db/update_gameweek_score.py -g 37
+poetry python src/db/update_gameweek_score.py -g 38
 
 
 
